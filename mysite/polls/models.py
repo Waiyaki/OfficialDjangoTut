b35e5e69fa4dee47baf7ed98f16105be6cb3332d
Waiyaki <muturijames3@gmail.com>
2015-02-22 00:54:25 +0300

    Made models.py PEP8 compliant

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,8 +3,10 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -15,8 +17,6 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = "Published recently?"
-
-
 
     def __str__(self):
         return self.question_text
@@ -29,4 +29,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
